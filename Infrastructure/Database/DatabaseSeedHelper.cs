bddb5d715ad9ac804f0324b2026d82ddcd5b01a0
cjarkemar <carljohan.arkemar@gmail.com>
2023-12-11 12:59:44 +0100

    RealDB fixed. Migration populating using Seed with Dogs

---
+++ Infrastructure/Database/DatabaseSeedHelper.cs
@@ -0,0 +1,33 @@
+﻿// DatabaseSeedHelper
+using System;
+using Domain.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Database
+{
+    public static class DatabaseSeedHelper
+    {
+        public static void SeedData(ModelBuilder modelBuilder)
+        {
+            SeedDogs(modelBuilder);
+
+            // Add more methods for other entities as needed
+        }
+
+        private static void SeedDogs(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Dog>().HasData(
+                new Dog { Id = Guid.NewGuid(), Name = "OldG" },
+                new Dog { Id = Guid.NewGuid(), Name = "NewG" },
+                new Dog { Id = Guid.NewGuid(), Name = "Björn" },
+                new Dog { Id = Guid.NewGuid(), Name = "Patrik" },
+                new Dog { Id = Guid.NewGuid(), Name = "Alfred" },
+                new Dog { Id = new Guid("12345678-1234-5678-1234-567812345671"), Name = "TestDogForUnitTests1" },
+                new Dog { Id = new Guid("12345678-1234-5678-1234-567812345672"), Name = "TestDogForUnitTests2" },
+                new Dog { Id = new Guid("12345678-1234-5678-1234-567812345673"), Name = "TestDogForUnitTests3" },
+                new Dog { Id = new Guid("12345678-1234-5678-1234-567812345674"), Name = "TestDogForUnitTests4" }
+            );
+        }
+    }
+}
+
---
+++ Infrastructure/Database/RealDatabase.cs
@@ -0,0 +1,39 @@
+﻿// RealDatabase
+using System;
+using Domain.Models;
+using Domain.Models.Animal;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Database
+{
+    public class RealDatabase : DbContext
+    {
+        public RealDatabase() { }
+        public RealDatabase(DbContextOptions<RealDatabase> options) : base(options) { }
+
+        public virtual DbSet<Dog> Dogs { get; set; }
+
+        public virtual DbSet<Cat> Cats { get; set; }
+
+        public virtual DbSet<Bird> Birds { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySql("server=localhost;port=3306;user=root;password=arkemar321;database=RealDB",
+                        new MySqlServerVersion(new Version(8, 2, 0)));
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // Call the SeedData method from the external class
+            DatabaseSeedHelper.SeedData(modelBuilder);
+        }
+    }
+
+}
+
+
+
